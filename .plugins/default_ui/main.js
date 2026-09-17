79c7aa70f59f20ae6a36dc531b12411da23c99a2
greenkod <greenkod.dev@gmail.com>
2026-09-17 23:11:03 +0300

    fix(devkit): ensure HUD initializes on startup and toggles on click without floor transition

--- .plugins/default_ui/main.js
+++ .plugins/default_ui/main.js
@@ -111,7 +111,11 @@ subscribeChannel("dock.ready", function() {
 });
 
 subscribeChannel("devkit_hud.ready", function() {
-    SnifferUI.forceUpdate();
+    SnifferUI.forceUpdate(true);
+});
+
+subscribeChannel("devkit_hud.toggleChanged", function() {
+    SnifferUI.forceUpdate(true);
 });
 
 subscribeChannel("dock.stateChanged", function() {
@@ -267,7 +271,7 @@ globalThis.onEvent = function (eventJsonString) {
             if (typeof callApi === "function") {
                 try {
                     callApi("devkit_hud.toggle");
-                    SnifferUI.forceUpdate();
+                    SnifferUI.forceUpdate(true);
                 } catch (e) {}
             }
             return "[]";
@@ -290,13 +294,16 @@ globalThis.onEvent = function (eventJsonString) {
 
 if (typeof subscribeChannel === "function") {
     subscribeChannel("dock.stateChanged", function() {
-        SnifferUI.forceUpdate();
+        SnifferUI.forceUpdate(true);
     });
     subscribeChannel("dock.ready", function() {
-        SnifferUI.forceUpdate();
+        SnifferUI.forceUpdate(true);
     });
     subscribeChannel("devkit_hud.ready", function() {
-        SnifferUI.forceUpdate();
+        SnifferUI.forceUpdate(true);
+    });
+    subscribeChannel("devkit_hud.toggleChanged", function() {
+        SnifferUI.forceUpdate(true);
     });
 }
 
@@ -305,7 +312,7 @@ SnifferUI.start(Root, state);
 if (typeof setInterval === "function") {
     setInterval(function() {
         if (state.showDevKitHud) {
-            SnifferUI.forceUpdate();
+            SnifferUI.forceUpdate(true);
         }
     }, 500);
 }
